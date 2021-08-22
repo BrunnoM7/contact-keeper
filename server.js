fb87de9a66ae62b5c523fd78d592941bdf0a467a
BrunnoM7 <brunno.marchetti7@gmail.com>
2021-08-22 13:30:10 -0300

    set up the api routes

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.json({ msg: 'Hello world!' })
 })
 
+// Define routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 
 const PORT = process.env.PORT || 5000;
