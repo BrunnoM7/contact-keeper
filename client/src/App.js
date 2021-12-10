425879645fd16e8c1297b2b858c59b273d9eb581
Brunno Marchetti <BrunnoM7@users.noreply.github.com>
2021-12-10 16:25:43 -0300

    created Login and Register components

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
 
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
@@ -21,7 +23,8 @@ const App = () => {
               <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
-
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
               </Switch>
             </div>
           </Fragment>
