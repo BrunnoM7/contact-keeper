00088035de36384bf65cf3a16a98c76d92ab7e46
Brunno Marchetti <BrunnoM7@users.noreply.github.com>
2021-12-13 15:24:05 -0300

    added update functionality connection to backend

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -64,6 +64,29 @@ const ContactState = props => {
     }    
   }
 
+  // Update Contact
+  const updateContact = async contact => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    try {
+      const res = await axios.put(`/api/contacts/${contact._id}`, contact, config);
+
+      dispatch({ 
+        type: UPDATE_CONTACT, 
+        payload: res.data 
+      });
+    } catch (err) {
+      dispatch({ 
+        type: CONTACT_ERROR,
+        payload: err.response.msg
+      });
+    }
+  }
+
   // Delete Contact
   const deleteContact = async id => {
 
@@ -87,11 +110,6 @@ const ContactState = props => {
   // Clear Current Contact
   const  clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT })
-  }
-
-  // Update Contact
-  const updateContact = contact => {
-    dispatch({ type:UPDATE_CONTACT, payload: contact })
   }
 
   // Clear Contacts
